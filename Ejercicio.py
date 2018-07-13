f0c2c517889bd9e3b2ea3b56074de201e73889ea
Darwin-Martinez <darmartinezvilla@gmail.com>
2018-07-13 12:18:59 -0500

    Practica1

---
+++ Ejercicio.py
@@ -0,0 +1,17 @@
+print ("hola mundo"); 
+print ("mi primer programa en Python"); 
+
+
+
+num1 = float(input("ingresa el primer numero"));
+num2 = float(input("ingresa el segundo numero"));
+
+total1 = num1+num2;
+total2 = num1-num2;
+total3 = num1*num2;
+total4 = num1/num2;
+
+print("el resultado de la suma es",total1);
+print("el resultado de la resta es",total2);
+print("el resultado de la multiplicacion es",total3);
+print("el resultado de la division es",total4);
